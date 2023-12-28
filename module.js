418f065654d5dbdd9cb49d40ecd582a0f6f3bc53
102-Funny <101511082+102-Funny@users.noreply.github.com>
2023-12-28 18:01:28 +0800

    Create module.js

---
+++ module.js
@@ -0,0 +1,20 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import {getAuth,GoogleAuthProvider,signInWithPopup,onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+import {getDatabase,ref,set,update,push,child,get,onValue,runTransaction} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAD5enlnyxoQ3j6aAuSN857Ohj75FwXF7c",
+    authDomain: "membership-system-4f4f8.firebaseapp.com",
+    databaseURL: "https://membership-system-4f4f8-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "membership-system-4f4f8",
+    storageBucket: "membership-system-4f4f8.appspot.com",
+    messagingSenderId: "737147849360",
+    appId: "1:737147849360:web:94e5a446393bbdfb8ae615",
+    measurementId: "G-9YR3MYPJEB"
+};
+/*firebase.initializeApp({
+  databaseURL: "https://membership-system-4f4f8.firebaseio.com/"
+});*/
+  
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
